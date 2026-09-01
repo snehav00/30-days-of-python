b99538acb80da6ecf79bca5fccfdff67b7ee8191
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-01 08:35:44 +0530

    Comparison logic and guessing loop

--- day-01-number-guessing/main.py
+++ day-01-number-guessing/main.py
@@ -3,3 +3,17 @@ import random
 print("I'm thinking of a number between 1 and 100")
 number = random.randint(1, 100)
 
+while True:
+    try:
+        guess = int(input("Enter your guess")) 
+        if 1 > guess > 100:
+            print("Out of range! Try again")
+        elif guess == number:
+            print("Correct!")
+            breakpoint
+        elif guess < number:
+            print("Too low")
+        else:
+            print("Too high")
+    except ValueError:
+        print("This is not a valid integer.")
