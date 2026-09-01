90ffc6695fb4dfb3b4ded8e46953355f7cb7dc75
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-01 08:50:11 +0530

    Added attempt counter

--- day-01-number-guessing/main.py
+++ day-01-number-guessing/main.py
@@ -2,18 +2,20 @@ import random
 
 print("I'm thinking of a number between 1 and 100")
 number = random.randint(1, 100)
-
+attempt = 0;
 while True:
     try:
-        guess = int(input("Enter your guess")) 
+        attempt += 1
+        guess = int(input(f"guess #{attempt}: ")) 
         if 1 > guess > 100:
             print("Out of range! Try again")
         elif guess == number:
             print("Correct!")
-            breakpoint
+            print(f"You got it in {attempt} attempts.")
+            break
         elif guess < number:
-            print("Too low")
+            print("Too low!")
         else:
-            print("Too high")
+            print("Too high!")
     except ValueError:
         print("This is not a valid integer.")
