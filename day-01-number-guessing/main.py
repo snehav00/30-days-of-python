62e062b7749fe1d10fbe280c86a291bb019dbe9a
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-01 09:19:20 +0530

    Complete number guessing game

--- day-01-number-guessing/main.py
+++ day-01-number-guessing/main.py
@@ -1,21 +1,28 @@
 import random
 
-print("I'm thinking of a number between 1 and 100")
-number = random.randint(1, 100)
-attempt = 0;
-while True:
-    try:
-        attempt += 1
-        guess = int(input(f"guess #{attempt}: ")) 
-        if 1 > guess > 100:
-            print("Out of range! Try again")
-        elif guess == number:
-            print("Correct!")
-            print(f"You got it in {attempt} attempts.")
-            break
-        elif guess < number:
-            print("Too low!")
-        else:
-            print("Too high!")
-    except ValueError:
-        print("This is not a valid integer.")
+print("Welcome to the number guessing game!\nI'm thinking of a number between 1 and 100.\nCan you guess it?")
+
+play = 'y'
+while play.lower() == 'y':
+    number = random.randint(1, 100)
+    attempt = 1
+    while True:
+        try:
+            guess = int(input(f"guess #{attempt}: ")) 
+            if guess < 1 or guess > 100:
+                print("Out of range! Try again")
+            elif guess == number:
+                print("Correct!")
+                print(f"You got it in {attempt} attempts.")
+                break
+            elif guess < number:
+                print("Too low!")
+                attempt += 1
+            else:
+                print("Too high!")
+                attempt += 1
+        except ValueError:
+            print("This is not a valid integer.")
+            
+    play = input("Play again? (y/n): ")
+print("Thanks for playing!")
