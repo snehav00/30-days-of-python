61fc5b6d1ee7f9d4a6448b105b0a5e64f3fb70cc
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-02 13:33:29 +0530

    Add exit option

--- day-02-basic-calculator/main.py
+++ day-02-basic-calculator/main.py
@@ -1,7 +1,5 @@
 print("=====================\n  PYTHON CALCULATOR\n=====================\n")
-a = int(input("Enter first number: "))
-operator = input("Enter operator(+, -, *, /): ")
-b = int(input("Enter second number: "))
+
 
 def calculate(a, operator, b):
     match operator:
@@ -17,6 +15,19 @@ def calculate(a, operator, b):
             print("INVALID OPERATOR!")
             return
 
+try: 
+    again = 'y'
+    use_answer = 'n'
+    while again != 'x':
         
-print(f"\nResult: {calculate(a, operator, b)}")
-    
+        a = int(input("Enter first number: "))
+        operator = input("Enter operator(+, -, *, /): ")
+        b = int(input("Enter second number: "))
+        result = calculate(a, operator, b)
+        print(f"\nResult: {result}")
+        
+        again = (input("Enter 'x' to exit the calculator and any key to continue: ")).lower()
+
+except ValueError:
+    print("INVALID NUMBER")
+
