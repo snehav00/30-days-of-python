4b7a131f2350ce26390bf000c48690e170eec9f4
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-02 09:34:29 +0530

    Add calculator function

--- day-02-basic-calculator/main.py
+++ day-02-basic-calculator/main.py
@@ -3,6 +3,20 @@ a = int(input("Enter first number: "))
 operator = input("Enter operator(+, -, *, /): ")
 b = int(input("Enter second number: "))
 
+def calculate(a, operator, b):
+    match operator:
+        case "+":
+            return a+b
+        case "-":
+            return a-b
+        case "*": 
+            return a*b
+        case "/":
+            return a/b
+        case _:
+            print("INVALID OPERATOR!")
+            return
 
         
+print(f"\nResult: {calculate(a, operator, b)}")
     
