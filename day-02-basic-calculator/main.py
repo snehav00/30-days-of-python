dc15e84138f28bf7300640c6967d00f477e23734
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-02 09:17:24 +0530

    Add user input

---
+++ day-02-basic-calculator/main.py
@@ -0,0 +1,8 @@
+print("=====================\n  PYTHON CALCULATOR\n=====================\n")
+a = int(input("Enter first number: "))
+operator = input("Enter operator(+, -, *, /): ")
+b = int(input("Enter second number: "))
+
+
+        
+    
