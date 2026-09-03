9b12631feef424a05ee66d47205b08a64078707a
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-03 16:53:18 +0530

    Add score tracking

--- day-03-Rock-Paper-Scissors/main.py
+++ day-03-Rock-Paper-Scissors/main.py
@@ -4,25 +4,30 @@ print("Welcome to Rock Paper Scissors!")
 
 play_again = 'y'
 choices = ["rock", "paper", "scissors"]
-
+user_wins = computer_wins = ties = 0
 while play_again == 'y':  
-    user_choice = input("Your choice: ").lower()
+    user_choice = input("\nYour choice: ").strip().lower()
     if user_choice not in choices:
         print("Invalid input! Choose rock, paper or scissors")
         continue
     computer_choice = random.choice(choices)
     print(f"Computer choice: {computer_choice}")
     
-    x = choices.index(computer_choice)
-    y = choices.index(user_choice)
+    computer_index = choices.index(computer_choice)
+    user_index = choices.index(user_choice)
     
-    if y > x or (y == 0 and x == 2):
-        print("You win!")
-    elif x == y:
-        print("Tie")
+    if user_index > computer_index or (user_index == 0 and computer_index == 2):
+        print("\nYou win!")
+        user_wins += 1
+    elif computer_index == user_index:
+        print("\nTie")
+        ties += 1
     else:
-        print("Computer win!")
+        print("\nComputer wins!")
+        computer_wins += 1
     
-    play_again = input("Do you want to play again(y/n): ")
+    play_again = input("\nDo you want to play again?(y/n): ").lower()
     
+print(f"Final scores\n\nYou: {user_wins}\nComputer: {computer_wins}\nTies: {ties}")
+
     
