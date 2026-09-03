d3143f95347ec13f377c1b2ceff1529ecd9cdd98
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-03 16:24:09 +0530

    Add game result logic

--- day-03-Rock-Paper-Scissors/main.py
+++ day-03-Rock-Paper-Scissors/main.py
@@ -13,4 +13,16 @@ while play_again == 'y':
     computer_choice = random.choice(choices)
     print(f"Computer choice: {computer_choice}")
     
+    x = choices.index(computer_choice)
+    y = choices.index(user_choice)
+    
+    if y > x or (y == 0 and x == 2):
+        print("You win!")
+    elif x == y:
+        print("Tie")
+    else:
+        print("Computer win!")
+    
+    play_again = input("Do you want to play again(y/n): ")
+    
     
