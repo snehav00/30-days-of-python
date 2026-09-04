c8fb37f5a450ebb907bee5885b6daf1c9b6a6554
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-04 20:40:29 +0530

    Add question display

--- day-04-quiz-game/main.py
+++ day-04-quiz-game/main.py
@@ -2,59 +2,71 @@ import random
 
 movies_quiz = [
     {
-        "question": "In Notting Hill, what kind of shop does William own?"
-        "choices": "Bookstore, Travel bookstore, Music store, Antique bookstore"
+        "question": "In Notting Hill, what kind of shop does William own?",
+        "choices": "Bookstore, Travel bookstore, Music store, Antique bookstore",
         "answer": "Travel bookstore"
     },
     {
-        "question": "In 27 Dresses, what does Kevin initially do for a living?"
-        "choices": "Writes about weddings, Works as a photographer, Runs a wedding venue, Works for a bridal magazine"
+        "question": "In 27 Dresses, what does Kevin initially do for a living?",
+        "choices": "Writes about weddings, Works as a photographer, Runs a wedding venue, Works for a bridal magazine",
         "answer": "Writes about weddings"
     },
     {
-        "question": "In The Proposal, what is Margaret Tate's position at the publishing company?"
-        "choices": "Editor-in-chief, Senior editor, Executive assistant, Publishing director"
+        "question": "In The Proposal, what is Margaret Tate's position at the publishing company?",
+        "choices": "Editor-in-chief, Senior editor, Executive assistant, Publishing director",
         "answer": "Senior editor"
     },
     {
-        "question": "In How to Lose a Guy in 10 Days, what does Andie Anderson write about?"
-        "choices": "Relationships and fashion, Lifestyle and relationships, Celebrity news, Travel and dating"
+        "question": "In How to Lose a Guy in 10 Days, what does Andie Anderson write about?",
+        "choices": "Relationships and fashion, Lifestyle and relationships, Celebrity news, Travel and dating",
         "answer": "Lifestyle and relationships"
     },
     {
-        "question": "In 10 Things I Hate About You, what instrument does Kat play?"
-        "choices": "Guitar, Piano, Violin, Drums"
+        "question": "In 10 Things I Hate About You, what instrument does Kat play?",
+        "choices": "Guitar, Piano, Violin, Drums",
         "answer": "Guitar"
     } 
 ]
-maths_quiz = [
+math_quiz = [
     {
-        "question": "10*5-5*0+5"
-        "choices": "50, 55, 45, 0"
+        "question": "10*5-5*0+5",
+        "choices": "50, 55, 45, 0",
         "answer": "55"
     },
     {
-        "question": "11*11+9"
-        "choices": "130, 139, 120, 129"
+        "question": "11*11+9",
+        "choices": "130, 139, 120, 129",
         "answer": "130"
     },
     {
-        "question": "10+45/5"
-        "choices": "10, 11, 15, 19"
+        "question": "10+45/5",
+        "choices": "10, 11, 15, 19",
         "answer": "19"
     },
     {
-        "question": "4+2"
-        "choices": "6, 4, 8, 10"
+        "question": "4+2",
+        "choices": "6, 4, 8, 10",
         "answer": "6"
     },
     {
-        "question": "3*2+0+5"
-        "choices": "14, 10, 15, 11"
+        "question": "3*2+0+5",
+        "choices": "14, 10, 15, 11",
         "answer": "11"
-    }        
+    }       
 ]
 
-
+random.shuffle(movies_quiz)
+print("=============\n|Movies Quiz|\n=============")
+for i, ques in enumerate(movies_quiz):
+    print(f"Question {i+1}: {ques["question"]}")
+    letters = "ABCD"
+    for j, choice in enumerate(ques["choices"].split(", ")):
+        print(f"{letters[j]}) {choice}")
     
-    
+random.shuffle(math_quiz)
+print("=============\n|Math Quiz|\n=============")
+for i, ques in enumerate(math_quiz):
+    print(f"Question {i+1}: {ques["question"]}")
+    letters = "ABCD"
+    for j, choice in enumerate(ques["choices"].split(", ")):
+        print(f"{letters[j]}) {choice}")
