d60ae2bc48a1428b045818c43a49c1ab1062c0c6
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-06 14:09:28 +0530

    Add character sets

--- day-05-password-generator/main.py
+++ day-05-password-generator/main.py
@@ -1,2 +1,25 @@
+import random 
+import string
+
+print("PASSWORD GENERATOR\n")
+
+while True:
+    try:
+        length = int(input("\nEnter password length(8 to 64): "))
+    except ValueError:
+        print("Enter valid length")
+        continue
+    if length>=8 and length<=64:
+        break
+    print("Password length must be between 8 and 64. ")
+
+uppercase = string.ascii_uppercase
+lowercase = string.ascii_lowercase
+numbers = string.digits
+special = string.punctuation
+
+all_chars = uppercase+lowercase+numbers+special
+
+
 
 
