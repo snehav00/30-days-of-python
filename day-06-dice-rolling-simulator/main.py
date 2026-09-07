8509de21ba0549e25d81b6559a9550d9793fbeb4
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-07 18:43:46 +0530

    Add dice input

--- day-06-dice-rolling-simulator/main.py
+++ day-06-dice-rolling-simulator/main.py
@@ -1,2 +1,17 @@
+import random
 
+print("Dice rolling simulator\n")
+def dice_number():
+    dice = input("Enter number of dice: ")
+    
+    
+def sides():
+    sides = input("Enter number of sides in a die: ")
+    
+
+
+
+
+dice_number()
+sides()
 
