440da9128d2cdfe7eecf047986f0db0a2ca1eef6
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-08 17:37:40 +0530

    Add task creation

--- day-07-to-do-list/main.py
+++ day-07-to-do-list/main.py
@@ -1,1 +1,10 @@
 print("==== TO-DO LIST ====\n")
+
+all_tasks = []
+
+def add_task(all_tasks):
+    task = input("Enter task to add: ")
+    new_task = {"task": task, "done": False}
+    all_tasks.append(new_task)
+    return all_tasks
+
