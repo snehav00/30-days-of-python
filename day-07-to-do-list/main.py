63849c6bd15735e9ef72d5edb3282381dc5cbd2e
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-08 20:32:45 +0530

    Add input validation

--- day-07-to-do-list/main.py
+++ day-07-to-do-list/main.py
@@ -3,11 +3,19 @@ print("==== TO-DO LIST ====\n")
 all_tasks = []
 
 def add_task(all_tasks):
-    task = input("Enter task to add: ").strip()
-    new_task = {"task": task, "done": False}
-    all_tasks.append(new_task)
+    while True:
+        task = input("Enter task to add: ")
+        if task:
+            new_task = {"task": task, "done": False}
+            all_tasks.append(new_task)
+            break
+        print("Task cannot be empty")
 
 def display(all_tasks):
+    if not all_tasks:
+        print("No tasks yet.\n")
+        return
+    print("\nCurrent Status:")
     for i, item in enumerate(all_tasks):
         if item['done']:
             print(f"Task {i+1}: {item['task']} \u2713")
@@ -38,6 +46,30 @@ def complete_task(all_tasks):
         print(f"Task '{task['task']}' marked as completed")
     else:
         print(f"Task '{task['task']}' is already completed!")
-    
-    
 
+def menu(all_tasks):
+    while True:
+        print("\n1. Add task\n2. View tasks\n3. Complete task\n4. Delete task\n5. Exit\n")
+        try:
+            user_choice = int(input("\nChoose an option 1 to 5: "))
+            match user_choice:
+                case 1:
+                    add_task(all_tasks)
+                    display(all_tasks)
+                case 2:
+                    display(all_tasks)
+                case 3:
+                    complete_task(all_tasks)
+                    display(all_tasks)
+                case 4:
+                    delete_task(all_tasks)
+                    display(all_tasks)
+                case 5:
+                    return
+                case _:
+                    print("Invalid number choice!\n")
+        except ValueError:
+            print("Not a number!\n")
+            
+menu(all_tasks)
+print("Goodbye!")
