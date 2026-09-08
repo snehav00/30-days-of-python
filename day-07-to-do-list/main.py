ae1f8e5ff10d2003f870735b7d605884ca95a448
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-08 17:52:14 +0530

    Display all tasks with status

--- day-07-to-do-list/main.py
+++ day-07-to-do-list/main.py
@@ -6,5 +6,14 @@ def add_task(all_tasks):
     task = input("Enter task to add: ")
     new_task = {"task": task, "done": False}
     all_tasks.append(new_task)
-    return all_tasks
 
+def display(all_tasks):
+    for i, item in enumerate(all_tasks):
+        if item['done']:
+            print(f"Task {i+1}: {item['task']} \u2713")
+        else:    
+            print(f"Task {i+1}: {item['task']}")
+
+
+add_task(all_tasks)
+display(all_tasks)
