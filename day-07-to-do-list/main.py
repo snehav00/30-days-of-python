35815158c1ca05d7982bbab8d5397de6c7c325fd
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-08 18:35:53 +0530

    Add task completion

--- day-07-to-do-list/main.py
+++ day-07-to-do-list/main.py
@@ -3,7 +3,7 @@ print("==== TO-DO LIST ====\n")
 all_tasks = []
 
 def add_task(all_tasks):
-    task = input("Enter task to add: ")
+    task = input("Enter task to add: ").strip()
     new_task = {"task": task, "done": False}
     all_tasks.append(new_task)
 
@@ -14,21 +14,30 @@ def display(all_tasks):
         else:    
             print(f"Task {i+1}: {item['task']}")
 
-def delete_task(all_tasks):
+def ask_task_number(length):
     while True:
         try:
-            task_number = int(input("\nEnter task number to delete: "))
+            task_number = int(input("\nTask number: "))
             
-            if task_number < 1 or task_number > len(all_tasks):
-                print("Invalid task number!")
-                continue
-            break
+            if task_number >= 1 and task_number <= length:
+                return task_number
+            print("Invalid task number!\n")
         except ValueError:
-            print("Invalid task number!")
+            print("Invalid task number!\n")
+
+def delete_task(all_tasks):
+    task_number = ask_task_number(len(all_tasks))
     deleted = all_tasks.pop(task_number-1)
     print(f"'{deleted['task']}' removed\n")
     
+def complete_task(all_tasks):
+    task_number = ask_task_number(len(all_tasks))
+    task = all_tasks[task_number-1]
+    if not task['done']:
+        task['done'] = True
+        print(f"Task '{task['task']}' marked as completed")
+    else:
+        print(f"Task '{task['task']}' is already completed!")
+    
+    
 
-add_task(all_tasks)
-display(all_tasks)
-delete_task(all_tasks)
