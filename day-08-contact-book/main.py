0540f80c5a9a06b388f2e6d10bc6d82b40ac7a04
Sneha Verma <147529314+snehav00@users.noreply.github.com>
2026-09-10 20:05:57 +0530

    Add menu

--- day-08-contact-book/main.py
+++ day-08-contact-book/main.py
@@ -1,1 +1,23 @@
+print("==== CONTACT BOOK ====\n")
 
+contacts = []
+
+while True:
+    print("""\n1. Add a contact
+          2. View contacts
+          3. Search contact
+          4. Update a contact
+          5. Delete a contact
+          6. Exit
+          """)
+    try:
+        option = int(input("\nChoose an option(1-5): "))
+        if 1 <= option <= 5:
+            break;
+        print("Invalid option!")
+    except ValueError:
+        print("Invalid number!")
+
+
+
+    
